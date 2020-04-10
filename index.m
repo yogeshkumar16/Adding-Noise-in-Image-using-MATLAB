5ce49cfbe039890f05b6e0407581a90934fc24f2
Yogesh K. Jangid <60933154+yogeshkumar16@users.noreply.github.com>
2020-04-10 19:55:50 +0530

    Add files via upload

--- index.m
+++ index.m
@@ -8,27 +8,17 @@ im=imread(fname);
 figure
 imshow(im);
 title('Real Image');
-%%Salt and Pepper Noise
+%%Noise
 for(i=1:3)
-im2(:,:,i)=imnoise(im(:,:,i),'salt & pepper',0.6);
+im2(:,:,i)=imnoise(im(:,:,i),'salt & pepper',.02);
 end
 figure
 imshow(im2);
-title(' Salt and Pepper Noisy Image');
-%Gausian noise with mean 0 and varience 0.08
+title('Noisy Image');
+%%Median Filter
 for(i=1:3)
-    im3(:,:,i)=imnoise(im(:,:,i),'gaussian',0.08);
+    im3(:,:,i)=medfilt2(im2(:,:,i),[3,3]);
 end
 figure
-imshow(im3);
-title('Gausian noise Image');
-%Speckle Noise with mean 0 and varience 0.05
-% Speckle Noise works according to the equation J=I+n*I,where n is
-% uniformly distributed random noise wiht mean 0 and varience V.
-%J=imnoise(I,'spekle',V)
-for(i=1:3)
-    im4(:,:,i)=imnoise(im(:,:,i),'speckle',0.05);
-end
-figure
-imshow(im4);
-title('Speckle Noise Image')
+imshow(im2);
+title('Median Filtered Image');
